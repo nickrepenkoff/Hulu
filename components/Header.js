3f0088cc0e02cf08752f4e5b24fe4cb9d587fa37
Nickrepenkoff <nikita.repenko@gmail.com>
2022-05-25 13:08:47 +0200

    deploy

---
+++ components/Header.js
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import HeaderItem from "./HeaderItem";
+import {HomeIcon, CollectionIcon, BadgeCheckIcon, SearchIcon, LightningBoltIcon, UserIcon} from "@heroicons/react/outline";
+
+const Header = () => {
+    const icons = [
+        {
+            id: 1,
+            title: 'HOME',
+            icon: HomeIcon
+        },
+        {
+            id: 2,
+            title: 'TRENDING',
+            icon: LightningBoltIcon
+        },
+        {
+            id: 3,
+            title: 'VERIFIED',
+            icon: BadgeCheckIcon
+        },
+        {
+            id: 4,
+            title: 'COLLECTIONS',
+            icon: CollectionIcon
+        },
+        {
+            id: 5,
+            title: 'SEARCH',
+            icon: SearchIcon
+        },
+        {
+            id: 6,
+            title: 'ACCOUNT',
+            icon: UserIcon
+        },
+
+
+
+
+    ]
+    return (
+        <header className='flex flex-col sm:flex-row m-5 justify-between items-center h-auto'>
+            <div className='flex flex-grow justify-evenly max-w-2xl'>
+                {icons.map(icon => (
+                    <HeaderItem key={icon.id} title={icon.title} Icon={icon.icon}/>
+                ))}
+            </div>
+            <Image
+                src='https://links.papareact.com/ua6'
+                width={200}
+                height={100}
+                className='object-contain'
+            />
+        </header>
+    );
+};
+
+export default Header;
---
+++ components/MainContent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Thumbnail from "./Thumbnail";
+import FlipMove from "react-flip-move";
+
+
+const MainContent = ({results}) => {
+    return (
+        <FlipMove className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center '>
+            {results.map(res => (
+                <Thumbnail key={res.id} result={res}/>
+            ))}
+        </FlipMove>
+    );
+};
+
+export default MainContent;
